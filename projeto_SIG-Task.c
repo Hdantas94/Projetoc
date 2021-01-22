6ea2dab139436cb15aa22ab68cc1a781435324b8
Hdantas94 <hilariod94@gmail.com>
2021-01-22 13:22:12 -0300

    update Sig-Task

---
+++ projeto_SIG-Task.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void menuSobre(void);
+
+int main(void) {
+    menuSobre();
+    return 0;
+
+}
+
+
+void menuSobre(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Serido                     ///\n");
+    printf("///               Departamento de Computacao e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programacao                      ///\n");
+    printf("///               Projeto Agenda de Tarefas e Compromissos                  ///\n");
+    printf("///                Developed by  @Hdantas94 - Jan, 2021                     ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///    = = = = = SIG-Task Agenda de Tarefas e Compromissos = = = = =        ///\n");
+    printf("///                                                                         ///\n");
+    printf("///  Programa exemplo utilizado na disciplina DCT1106 - Programacao, para   ///\n");
+    printf("///  fins didaticos de ilustracao. O programa contem os principais modulos  ///\n");
+    printf("///  e funcionalidades que serao exigidos na avaliacao dos projetos a serem ///\n");
+    printf("///  desenvolvidos pelos alunos ao longo da disciplina. Serve como um guia  ///\n");
+    printf("///  de consulta e referencia para o desenvolvidos dos demais projetos.     ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
