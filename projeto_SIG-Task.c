aa1f4a78f0e95358e312b88c59ab20a706b2c0fa
Hdantas94 <hilariod94@gmail.com>
2021-03-01 23:35:54 -0300

    commit 5/15

--- projeto_SIG-Task.c
+++ projeto_SIG-Task.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void menuSobre(void);
+
+
 char menuPrincipal(void);
-char menuTarefa(void);
-void moduloTarefa(void);
-char menuCompromisso(void);
-void moduloCompromisso(void);
-void telaCadastrarTarefa(void);
-void telaCadastrarCompromisso(void);
 void moduloInformacoes(void);
+void menuSobre(void);
+
+void moduloTarefa(void);
+char menuTarefa(void);
+void telaCadastrarTarefa(void);
 void pesquisarTarefa(void);
 void atualizarTarefa(void);
 void excluirTarefa(void);
+
+char menuCompromisso(void);
+void moduloCompromisso(void);
+void telaCadastrarCompromisso(void);
 void pesquisarCompromisso(void);
 void atualizarCompromisso(void);
 void excluirCompromisso(void);
@@ -137,45 +141,6 @@ char menuTarefa(void) {
 	return op;
 }
 
-
-
-char menuCompromisso(void) {
-	char op;
-    system("cls");
-	printf("\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///          ===================================================          ///\n");
-	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-	printf("///          = = = =   Agenda de Tarefas e Compromissos  = = = =          ///\n");
-	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-	printf("///          ===================================================          ///\n");
-	printf("///                Developed by  @Hdantas94 - Jan, 2021                   ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///           = = = = = = =  Menu compromisso   = = = = = = =             ///\n");
-	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-	printf("///                                                                       ///\n");
-	printf("///           1. Cadastrar um novo compromisso                            ///\n");
-	printf("///           2. Pesquisar por compromisso ja cadastrado                  ///\n");
-	printf("///           3. Atualizar dados de um compromisso                        ///\n");
-	printf("///           4. Excluir um um compromisso do sistema                     ///\n");
-	printf("///           0. Voltar ao menu anterior                                  ///\n");
-	printf("///                                                                       ///\n");
-	printf("///           Escolha a opcao desejada:                                   ///\n");
-	scanf("%c", &op);
-	printf("///                                                                       ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("\n");
-    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-	getchar();
-	return op;
-}
-
-
 void telaCadastrarTarefa(void) {
 	char tipo[51];
 	char desc[200];
@@ -226,6 +191,47 @@ void telaCadastrarTarefa(void) {
 	printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
 	getchar();
 }
+
+
+
+char menuCompromisso(void) {
+	char op;
+    system("cls");
+	printf("\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          = = = =   Agenda de Tarefas e Compromissos  = = = =          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///                Developed by  @Hdantas94 - Jan, 2021                   ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///           = = = = = = =  Menu compromisso   = = = = = = =             ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///                                                                       ///\n");
+	printf("///           1. Cadastrar um novo compromisso                            ///\n");
+	printf("///           2. Pesquisar por compromisso ja cadastrado                  ///\n");
+	printf("///           3. Atualizar dados de um compromisso                        ///\n");
+	printf("///           4. Excluir um um compromisso do sistema                     ///\n");
+	printf("///           0. Voltar ao menu anterior                                  ///\n");
+	printf("///                                                                       ///\n");
+	printf("///           Escolha a opcao desejada:                                   ///\n");
+	scanf("%c", &op);
+	printf("///                                                                       ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+	return op;
+}
+
+
+
 
 void telaCadastrarCompromisso(void) {
 	char tipo[51];
