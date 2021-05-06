151f3411f9ebe173a381ada5862359d4a67f3f8e
Hdantas94 <hilariod94@gmail.com>
2021-05-06 00:55:15 -0300

    update

--- tarefa.c
+++ tarefa.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "tarefa.h"
+#include "funcoes.h"
 
 
 void moduloTarefa(void) {
@@ -19,6 +21,60 @@ void moduloTarefa(void) {
 		} 		
 	} while (opcao != '0');
 }
+
+void cadastrarTarefa(void) {
+	Tarefa* trf;
+
+	trf = telaCadastrarTarefa();
+	gravarTarefa(trf);
+	free(trf);
+}
+
+void pesquisarTarefa(void) {
+	Tarefa* trf;
+	char* codRespons;
+
+	codRespons = telaPesquisarTarefa();
+	trf = buscarTarefa(codRespons);
+	exibirTarefa(trf);
+	free(trf);
+	free(codRespons);
+}
+
+void atualizarTarefa(void) {
+	Tarefa* trf;
+	char* codRespons;
+
+	codRespons = telaAtualizarTarefa();
+	trf = buscarTarefa(codRespons);
+	if (trf == NULL) {
+    	printf("\n\nTarefa não encontrada!\n\n");
+  	} else {
+		trf = telaCadastrarTarefa();
+		strcpy(trf->codRespons, codRespons);
+		regravarTarefa(trf);
+		free(trf);
+	}
+	free(codRespons);
+}
+
+void excluirTarefa(void) {
+	Tarefa* trf;
+	char *codRespons;
+
+	codRespons = telaExcluirTarefa();
+	trf = (Tarefa*) malloc(sizeof(Tarefa));
+	trf = buscarTarefa(codRespons);
+	if (trf == NULL) {
+    	printf("\n\nTarefa não encontrada!\n\n");
+  	} else {
+		  trf->status = False;
+		  regravarTarefa(trf);
+		  free(trf);
+	}
+	free(codRespons);
+}
+
 
 
 char menuTarefa(void) {
@@ -46,19 +102,45 @@ char menuTarefa(void) {
 	printf("///           4. Excluir dados de uma tarefa                              ///\n");
 	printf("///           0. Voltar ao menu anterior                                  ///\n");
 	printf("///                                                                       ///\n");
-	printf("///           Escolha a opcao desejada:                                   ///\n");
+	printf("///           Escolha a opcao desejada: ");
 	scanf("%c", &op);
 	printf("///                                                                       ///\n");
 	printf("///                                                                       ///\n");
 	printf("/////////////////////////////////////////////////////////////////////////////\n");
 	printf("\n");
-    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
 	getchar();
 	return op;
 }
 
-void cadastrarTarefa(void) {
+void telaErroArquivo(void) {
+	system("cls");
+	printf("\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          = = = =  Agenda de Tarefas e Compromissos   = = = =          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///                Developed by  @Hdantas94 - 2021                        ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///           = = = = = = =  Ops! Ocorreu em erro = = = = = =             ///\n");
+	printf("///           = = =  Não foi possível acessar o arquivo = = =             ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///           = = =   Este programa será finalizado!    = = =             ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	getchar();
+	exit(1);
+}
+
+Tarefa* telacadastrarTarefa(void) {
 	Tarefa *trf;
+	trf = (Tarefa*) malloc(sizeof(Tarefa));
 
     system("cls");
 	printf("\n");
@@ -77,42 +159,187 @@ void cadastrarTarefa(void) {
 	printf("///           = = = = = = = = Cadastrar Tarefa  = = = = = = =             ///\n");
 	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
 	printf("///                                                                       ///\n");
-	trf = (Tarefa*) malloc(sizeof(Tarefa));
-	printf("///           Tipo:                                                       ///\n");
+	printf("///           Tipo: ");
 	scanf("%[^\n]", trf->tipo);
 	getchar();
-	printf("///           Descricao:                                                  ///\n");
-	scanf("%[^\n]", trf->desc);
-	getchar();
-	printf("///           Responsavel:                                                ///\n");
+	printf("///          CPF do Responsavel: ");
 	scanf("%[^\n]", trf->codRespons);
 	getchar();
-	printf("///           Data:                                                       ///\n"); 
+	printf("///           Data: "); 
 	scanf("%[^\n]", trf->data);
 	getchar();
-	printf("///           Horário:                                                       ///\n"); 
+	printf("///           Horário: "); 
 	scanf("%[^\n]", trf->horario);
 	getchar();
-	printf("///           Prioridade:                                                 ///\n");
+	printf("///           Prioridade: ");
 	scanf("%[^\n]", trf->prioridade);
 	getchar();
 	printf("///                                                                       ///\n");
 	printf("///                                                                       ///\n");
 	printf("/////////////////////////////////////////////////////////////////////////////\n");
 	printf("\n");
-	printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	getchar();
+	return trf;
+}
+
+char* telaPesquisarTarefa(void) {
+	char* codRespons;
+	codRespons = (char*) malloc(12*sizeof(char));
+	
+	system("cls");
+	printf("\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          = = = =  Agenda de Tarefas e Compromissos   = = = =          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///                Developed by  @Hdantas94 - Jan, 2021                   ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///           = = = = = =  Pesquisar Tarefa       = = = = = =             ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///                                                                       ///\n");
+	printf("/// Informe o CPF do responsavel da tarefa que deseja pesquisar: ");
+	scanf("%[^\n]", codRespons);
+	getchar();
+	printf("///                                                                       ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+	return codRespons;
+}
+
+char* telaAtualizarTarefa(void) {
+	char* codRespons;
+	codRespons = (char*) malloc(12*sizeof(char));
+	
+	system("cls");
+	printf("\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          = = = =  Agenda de Tarefas e Compromissos   = = = =          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///                Developed by  @Hdantas94 - Jan, 2021                   ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///           = = = = = =  Atualizar tarefa = = = = = =             ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///                                                                       ///\n");
+	printf("/// Informe o CPF do responsavel da tarefa que deseja atualizar os dados: ");
+	scanf("%[^\n]", codRespons);
+	getchar();
+	printf("///                                                                       ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+	return codRespons;
+
+}
+
+char* telaExcluirTarefa(void) {
+	char* codRespons;
+	codRespons = (char*) malloc(12*sizeof(char));
+	
+	system("cls");
+	printf("\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          = = = =  Agenda de Tarefas e Compromissos   = = = =          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///                Developed by  @Hdantas94 - Jan, 2021                   ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///           = = = = = =   Excluir Tarefa        = = = = = =             ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///                                                                       ///\n");
+	printf("/// Informe o CPF do responsavel da tarefa que deseja excluir: ");
+	scanf("%[^\n]", codRespons);
+	getchar();
+	printf("///                                                                       ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+	return codRespons;
+
+}
+
+void gravarTarefa(Tarefa* trf) {
+	FILE* fp;
+
+	fp = fopen("tarefa.dat", "ab");
+	if (fp == NULL) {
+		telaErroArquivo();
+	}
+	fwrite(trf, sizeof(Tarefa), 1, fp);
+	fclose(fp);
+}
+
+Tarefa* buscarTarefa(char* codRespons) {
+	FILE* fp;
+	Tarefa* trf;
+
+	trf = (Tarefa*) malloc(sizeof(Tarefa));
+	fp = fopen("tarefa.dat", "rb");
+	if (fp == NULL) {
+		telaErroArquivo();
+	}
+	while(fread(trf, sizeof(Tarefa), 1, fp)) {
+		if ((strcmp(trf->codRespons, codRespons) == 0) && (trf->status == True)) {
+			fclose(fp);
+			return trf;
+		}
+	}
+	fclose(fp);
+	return NULL;
+}
+
+void exibirTarefa(Tarefa* trf) {
+
+	if (trf == NULL) {
+		printf("\n= = = Tarefa Nao Existe = = =\n");
+	} else {
+		printf("\n= = = Tarefa Cadastrada = = =\n");
+		printf("CPF Responsavel: %s\n", trf->codRespons);
+		printf("Data: %s\n", trf->data);
+		printf("Tipo: %d\n", trf->tipo);
+		printf("Horario: %s\n", trf->horario);
+		printf("Prioridade: %d\n", trf->prioridade);
+	}
 	getchar();
 }
 
-void pesquisarTarefa(void) {
+void regravarTarefa(Tarefa* trf) {
+	int achou;
+	FILE* fp;
+	Tarefa *trfLido;
 
+	trfLido = (Tarefa*) malloc(sizeof(Tarefa));
+	fp = fopen("tarefa.dat", "r+b");
+	if (fp == NULL) {
+		telaErroArquivo();
+	}
+	achou = False;
+	while(fread(trfLido, sizeof(Tarefa), 1, fp) && !achou) {
+		if (strcmp(trfLido->codRespons, trf->codRespons) == 0) {
+			achou = True;
+			fseek(fp, -1*sizeof(Tarefa), SEEK_CUR);
+        	fwrite(trf, sizeof(Tarefa), 1, fp);
+		}
+	}
+	fclose(fp);
+	free(trfLido);
 }
-
-void atualizarTarefa(void) {
-
-}
-
-void excluirTarefa(void) {
-
-}
-
--- tarefa.h
+++ tarefa.h
@@ -3,11 +3,11 @@ typedef struct tarefa Tarefa;
 
 struct tarefa {
   char tipo;
-  char desc[200];
   char codRespons[12];
   char data[9];
   char horario[12];
   char prioridade;
+  int status;
 };
 
 
@@ -16,7 +16,18 @@ struct tarefa {
 void moduloTarefa(void);
 char menuTarefa(void);
 
+Tarefa* telaCadastrarTarefa(void);
+char* telaPesquisarTarefa(void);
+char* telaAtualizarTarefa(void);
+char* telaExcluirTarefa(void);
+void telaErroArquivo(void);
+
 void cadastrarTarefa(void);
 void pesquisarTarefa(void);
 void atualizarTarefa(void);
 void excluirTarefa(void);
+void gravarTarefa(Tarefa*);
+
+void exibirTarefa(Tarefa*);
+Tarefa* buscarTarefa(char*);
+void regravarTarefa(Tarefa*);
